a02cbca91cdfa4756dad722706608f3c8ee6be31
Christian Vanderwall <christian@cvdub.net>
2021-02-26 20:59:52 -0800

    Update emacs script to work with doom emacsclient

--- scripts/emacs.sh
+++ scripts/emacs.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/sh
 
 emacsclient -e '(cjv-load-active-theme)' > /dev/null 2>&1
+
+DOOM_THEME=doom-"$THEME"
+if [ "$VARIANT" = "light" ]; then
+    DOOM_THEME="$DOOM_THEME"-light
+fi
+emacsclient -q -s doom -e "(counsel-load-theme-action \"$DOOM_THEME\")" 
