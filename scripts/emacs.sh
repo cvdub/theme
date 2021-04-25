861f6e55b676031bfc98ec18e1fc20304ded2222
Christian Vanderwall <christian@cvdub.net>
2021-04-24 18:59:17 -0700

    Revert "Update emacs script to work with doom emacsclient"
    This reverts commit a02cbca91cdfa4756dad722706608f3c8ee6be31.

--- scripts/emacs.sh
+++ scripts/emacs.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/sh
 
 emacsclient -e '(cjv-load-active-theme)' > /dev/null 2>&1
-
-DOOM_THEME=doom-"$THEME"
-if [ "$VARIANT" = "light" ]; then
-    DOOM_THEME="$DOOM_THEME"-light
-fi
-emacsclient -q -s doom -e "(counsel-load-theme-action \"$DOOM_THEME\")" 
