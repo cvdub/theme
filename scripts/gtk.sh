5fb3d2d73e6efcef0040e8899555bb9848a41de7
Christian Vanderwall <christian@cvdub.net>
2021-04-24 18:58:24 -0700

    Set theme for GTK4

--- scripts/gtk.sh
+++ scripts/gtk.sh
@@ -16,11 +16,13 @@ else
     CONFIG_DIR=~/.config
 fi
 
+GTK4_CONFIG_FILE=$CONFIG_DIR/gtk-4.0/settings.ini
 GTK3_CONFIG_FILE=$CONFIG_DIR/gtk-3.0/settings.ini
 GTK2_CONFIG_FILE=~/.gtkrc-2.0
 QT_CONFIG_FILE=$CONFIG_DIR/qt5ct/qt5ct.conf
 
 gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
+sed -i 's/gtk-theme-name=.*/gtk-theme-name='"$GTK_THEME"'/' "$GTK4_CONFIG_FILE"
 sed -i 's/gtk-theme-name=.*/gtk-theme-name='"$GTK_THEME"'/' "$GTK3_CONFIG_FILE"
 sed -i 's/gtk-theme-name=.*/gtk-theme-name="'"$GTK_THEME"'"/' "$GTK2_CONFIG_FILE"
 sed -i 's/style=.*/style='"$GTK_THEME"'/' $QT_CONFIG_FILE
